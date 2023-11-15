37bde3f76dbba3bc9d0a9406cc6b344c4d8b382f
ASUS <ASUS@DESKTOP-73R147E>
2023-11-15 20:49:30 +0330

    egrasgrsgsa

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -16,8 +16,13 @@ namespace ConsoleApp3
             bird bolbol=new bird();
             dog petbol=new dog();
             petbol.sar = "3";
+            ird bolbol = new bird();
+            dog petbol = new dog();
+            petbol.sar = "3";
+            klsldghskgn
+            klsldghskgn
+            zfZV
 
-            
         }
     }
 }
